7da487a1dfba85ebb5eccd4b231267611e2edff8
rackom <rackom10@gmail.com>
2011-10-23 10:47:59 +0200

    Upravene ziskavanie poli z PDF dokumentu

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,36 @@
-require 'pdf/toolkit'
-
 class Document < ActiveRecord::Base
   mount_uploader :pdffile, PdffileUploader
+  after_save :dfields
   
-  def self.dfields(doc)
-    PDF::Toolkit.pdftk(doc.to_s,"dump_data_fields","output","-",:mode => 'r')
+  def dfields
+    if not self.description?
+      # ziskat svoj zaznam
+      doc = self.pdffile
+    
+      # ziskat absolutnu cestu ku pdf suboru
+      url = %x[pwd] + "/public" + doc.url
+    
+      # odstranit \n ktore vracia pwd
+      url.gsub!(/\n/,'')
+
+      # ziskat datove polia
+      data_fields = %x[pdftk #{url} dump_data_fields]
+    
+      # nahradit newline za <br>
+      data_fields.gsub!(/\n/,'<br>')
+    
+      # aby to bolo v pohode s html
+      data_fields.html_safe
+      
+      #test
+      #data_fields.gsub!('---','')
+      #main_array = data_fields.split("\t")
+      #main_array.each {|element| element = element.split("\n") }
+      #test
+    
+      #self.description = main_array
+      self.description = data_fields
+      self.save
+    end
   end
 end
