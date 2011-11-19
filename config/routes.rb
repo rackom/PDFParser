da39263f1572d978cfea3e9fc8ec1cd9456352b2
rackom <rackom10@gmail.com>
2011-11-19 13:39:58 +0100

    Pridany vzhlad na CSS

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,8 @@ Pdfparser::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
+  root :to => 'documents#index'
 
   # See how all your routes lay out with "rake routes"
 
